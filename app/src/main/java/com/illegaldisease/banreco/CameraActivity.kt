2d10065132038c00dd215c5f59ea8db0823f5eea
Emiliya Ismayil <emiliyaismayil2014@gmail.com>
2018-04-12 21:05:42 +0300

    Add the nav drawer button to the app bar and open/close events

--- app/src/main/java/com/illegaldisease/banreco/CameraActivity.kt
+++ app/src/main/java/com/illegaldisease/banreco/CameraActivity.kt
@@ -1,12 +1,43 @@
 package com.illegaldisease.banreco
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+
 
 class CameraActivity : AppCompatActivity() {
+
+    private var mDrawerLayout: DrawerLayout? = null
+
+    private var mToggle: ActionBarDrawerToggle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+        mDrawerLayout = findViewById(R.id.drawer_layout) as DrawerLayout
+        mToggle = ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close)
+
+        mDrawerLayout!!.addDrawerListener(mToggle!!)
+        mToggle!!.syncState()
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
+
+
     }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+
+        if (mToggle!!.onOptionsItemSelected(item))
+
+                    return true ;
+
+
+                    return super.onOptionsItemSelected(item)
+
+
+}
 }
