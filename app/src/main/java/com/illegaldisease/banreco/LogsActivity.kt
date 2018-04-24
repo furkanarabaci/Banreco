68ebec6cede024eb72676472472b67915238d8ba
Furkan Arabacı <illegaldisease@gmail.com>
2018-04-24 23:24:00 +0300

    fixed actionbar thingy

--- app/src/main/java/com/illegaldisease/banreco/LogsActivity.kt
+++ app/src/main/java/com/illegaldisease/banreco/LogsActivity.kt
@@ -11,7 +11,6 @@ class LogsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logs)
-        setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
