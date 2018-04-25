1d47bfedf938118dee0337b9833111f2336644b7
Furkan Arabacı <illegaldisease@gmail.com>
2018-04-25 21:39:16 +0300

    Changes with navigation bar

--- app/src/main/java/com/illegaldisease/banreco/CameraActivity.kt
+++ app/src/main/java/com/illegaldisease/banreco/CameraActivity.kt
@@ -1,21 +1,19 @@
 package com.illegaldisease.banreco
 
-import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.NavigationView
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.view.WindowManager
+import co.zsmb.materialdrawerkt.builders.drawer
+import co.zsmb.materialdrawerkt.draweritems.badgeable.secondaryItem
+import co.zsmb.materialdrawerkt.draweritems.divider
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.mikepenz.materialdrawer.DrawerBuilder
-
-
 
 class CameraActivity : AppCompatActivity() {
 
     private var mGoogleSignInClient : GoogleSignInClient? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
@@ -23,15 +21,41 @@ class CameraActivity : AppCompatActivity() {
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build()
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        DrawerBuilder().withActivity(this).build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        initializeDrawerBar()
     }
-
     override fun onStart() {
         super.onStart()
         checkSignIn() // Attempts to login with async callbacks. be careful.
     }
-    fun checkSignIn(){
+    private fun initializeDrawerBar(){
+        //Be careful, i don't handle outofrange exceptions.
+        drawer {
+            //TODO: Add some icons and fill out onclick
+            secondaryItem(getString(R.string.drawer_events)) {
+
+            }
+            secondaryItem(getString(R.string.drawer_calendar)) {
+                icon = R.drawable.ic_date_range_black_24dp
+            }
+            secondaryItem(getString(R.string.drawer_settings)) {
+                icon = R.drawable.ic_settings_black_24dp
+            }
+            secondaryItem(getString(R.string.drawer_logs)) {
+                icon = R.drawable.ic_archive_black_24dp
+            }
+            secondaryItem(getString(R.string.drawer_about)) {
+                icon = R.drawable.ic_more_black_24dp
+            }
+            secondaryItem(getString(R.string.drawer_rate)) {
+                icon = R.drawable.ic_star_border_black_24dp
+            }
+            divider {  }
+            secondaryItem(getString(R.string.drawer_logout)) {
+            }
+        }
+    }
+    private fun checkSignIn(){
         val task = mGoogleSignInClient!!.silentSignIn()
         if (task.isSuccessful) {
             // There's immediate result available.
